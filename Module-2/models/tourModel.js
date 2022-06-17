32544370bc2c380df362a90b6e042f9daa2158b5
nasim67reja <nasimreja67@gmail.com>
2022-06-17 02:15:57 -0700

    First Commit

---
+++ Module-2/models/tourModel.js
@@ -0,0 +1,62 @@
+const monggose = require('mongoose');
+
+// Mongoose schema:
+
+const tourSchema = new monggose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+    trim: true
+  },
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration']
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size']
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficultry']
+  },
+  ratingsAverage: {
+    type: Number,
+    default: 4.5
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price']
+  },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a description']
+  },
+  description: {
+    type: String,
+    trim: true
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour have cover image']
+  },
+  images: [String],
+  createAt: {
+    type: Date,
+    default: Date.now()
+    // select: false
+  },
+  startDates: [Date]
+});
+
+// create model:
+const Tour = monggose.model('Tour', tourSchema);
+
+module.exports = Tour;
---
+++ Module-2/server.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB)
+  .then(() => console.log('DB connection successful!'))
+  .catch(err => {
+    console.log(err.message);
+  });
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
