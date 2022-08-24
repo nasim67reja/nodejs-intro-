387fa61749309d1fc2d82b2759ef6a013d7e20f4
Nasim Reja <nasimreja67@gmail.com>
2022-08-24 10:53:21 +0600

    Updated Data Modeling

---
+++ Data-modelling-updated/README.md
@@ -0,0 +1,97 @@
+# Data Modelling with Mongodb
+
+## Modelling Locations (Geospatial Data)
+
+Mongodb supports geo spatial data out of the box and geo spatial data is the data that basically describes places on earth using latitude and longitude co-ordinates.
+Mongodb uses a special data formates call geoJson in order to specify geospatial data.
+
+- For realizing mongodb that it is a geospaital data not schema type option, we need to do specify two fields `type` and `co-ordinates`
+
+```js
+
+startLocation: {
+      // GeoJSON
+      type: {
+        type: String,
+        default: 'Point',
+        enum: ['Point']
+      },
+      coordinates: [Number],
+      address: String,
+      description: String
+    },
+    locations: [
+      {
+        type: {
+          type: String,
+          default: 'Point',
+          enum: ['Point']
+        },
+        coordinates: [Number],
+        address: String,
+        description: String,
+        day: Number
+      }
+    ],
+
+```
+
+- That's actually it for now. we will use this for later in the sections for geo spatial query
+
+## Modelling Tour Guides :Embeding
+
+so the idea here is that when creating a new tour document the user will simple add an
+array of user id's. and we will then get the corresponding users documents based ont his id's and add them to our tour document.
+Now, How are gonna we do that?
+
+- make a Schema option `Guides:Array`
+
+- put this in the top of tourModel file `const User = require('./userModel');`
+
+- then this middlewawre
+
+```js
+tourSchema.pre('save', async function(next) {
+  const guidesPromises = this.guides.map(async id => await User.findById(id));
+  this.guides = await Promise.all(guidesPromises);
+  next();
+});
+```
+
+## Modelling Tour Guides : Child Referencing
+
+- Special Schema type option
+
+```js
+guides: [
+  {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User'
+  }
+];
+```
+
+**note that we actually even don't need the Usermodel here for referencing**
+
+- in the next lecture we will get the user data by a process call **Ppulating**
+
+## Populating Tour Guides
+
+- Explaination are exist in paper. check there
+
+- Go to the single Product get handler function and add there populate.
+
+```js
+const tour = await Tour.findById(req.params.id).populate('guides');
+```
+
+- We also remove some fields
+
+```js
+const tour = await Tour.findById(req.params.id).populate({
+  path: 'guides',
+  select: '-__v -passwordChangedAt'
+});
+```
+
+**Behind the scenes populate makes new query so don't use it in many places blindly ofcourse in large application**
---
+++ Data-modelling-updated/controllers/authController.js
@@ -0,0 +1,219 @@
+const crypto = require('crypto');
+const { promisify } = require('util');
+const jwt = require('jsonwebtoken');
+const User = require('./../models/userModel');
+const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/AppError');
+const sendEmail = require('./../utils/email');
+
+const signToken = id => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN
+  });
+};
+
+const createSendToken = (user, statusCode, res) => {
+  const token = signToken(user._id);
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true
+  };
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
+  res.cookie('jwt', token, cookieOptions);
+
+  // Remove password from output
+  user.password = undefined;
+
+  res.status(statusCode).json({
+    status: 'success',
+    token,
+    data: {
+      user
+    }
+  });
+};
+// ////////////////////////////////////////////////
+exports.signup = catchAsync(async (req, res, next) => {
+  // const newUser = await User.create(req.body);
+
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+    passwordChangedAt: req.body.passwordChangedAt
+  });
+
+  createSendToken(newUser, 201, res);
+});
+
+// ////////////////////////////////////////////////
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // 1) Check if email and password exist
+  if (!email || !password) {
+    return next(new AppError('Please provide email and password!', 400));
+  }
+  // 2) Check if user exists && password is correct
+  const user = await User.findOne({
+    email
+  }).select('+password');
+
+  if (!user || !(await user.correctPassword(password))) {
+    return next(new AppError('Incorrect email or password', 401));
+  }
+
+  // 3) If everything ok, send token to client
+  createSendToken(user, 200, res);
+});
+
+// ////////////////////////////////////////////////
+exports.protect = catchAsync(async (req, res, next) => {
+  //  1) Getting token and check if it's there
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+  if (!token) {
+    return next(
+      new AppError('You are not logged in! Please log in to get access.', 401)
+    );
+  }
+  // 2) Verification token
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+  // 3) Check if user still exists
+  const currentUser = await User.findById(decoded.id);
+
+  if (!currentUser) {
+    return next(
+      new AppError(
+        'The user belonging to this token does no longer exist.',
+        401
+      )
+    );
+  }
+
+  // 4) Check if user changed password after the JWT/token was issued
+  if (currentUser.changedPasswordAfter(decoded.iat)) {
+    return next(
+      new AppError('User recently changed password! Please log in again.', 401)
+    );
+  }
+
+  // GRANT ACCESS TO PROTECTED ROUTE
+  req.user = currentUser;
+  next();
+});
+
+exports.restrictTo = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new AppError('You do not have permission to perform this action', 403)
+      );
+    }
+    next();
+  };
+};
+
+exports.forgotPassword = catchAsync(async (req, res, next) => {
+  ////////// 1) Get user based on POSTed email
+  const user = await User.findOne({ email: req.body.email }); // because user give the email not the id
+  if (!user)
+    return next(new AppError('There is no user with email address', 404));
+
+  /////////// 2) Generate the random reset token
+  const resetToken = user.createPasswordResetToken(); // we just update the document by calling this function. that's why we need to save the document in database
+  await user.save({ validateBeforeSave: false });
+  // await user.save({ validateModifiedOnly: true }); // alternative way
+
+  /////////// 3) Send it to user's email
+  const resetURL = `${req.protocol}://${req.get(
+    'host'
+  )}/api/v1/users/resetPassword/${resetToken}`;
+
+  const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.\nIf you didn't forget your password, please ignore this email!`;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: 'Your password reset token (valid for 10 min)',
+      message
+    });
+
+    res.status(200).json({
+      status: 'success',
+      message: 'Token sent to email!'
+    });
+  } catch (err) {
+    user.passwordResetToken = undefined;
+    user.passwordResetExpires = undefined;
+    await user.save({ validateBeforeSave: false });
+
+    return next(
+      new AppError('There was an error sending the email. Try again later!'),
+      500
+    );
+  }
+});
+
+exports.resetPassword = catchAsync(async (req, res, next) => {
+  // 1) Get user based on the token
+  const hashedToken = crypto
+    .createHash('sha256')
+    .update(req.params.token)
+    .digest('hex');
+
+  const user = await User.findOne({
+    passwordResetToken: hashedToken,
+    passwordResetExpires: { $gt: Date.now() } // ekhnae amra user khujtechi hashedtoken mile jay  and p.R.E time ekhn kar thkee beshi emon user khujtechi
+  });
+
+  //  2) If token has not expired , and there is user , set the new password
+  if (!user) return next(new AppError('Token is Invalid or has expired', 400));
+
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.password;
+  user.passwordResetToken = undefined;
+  user.passwordResetExpires = undefined;
+
+  await user.save();
+  // 3) Update changePassword At property for the user
+
+  // 4) Log the user in, send JWT
+  createSendToken(user, 200, res);
+});
+
+exports.updatePassword = catchAsync(async (req, res, next) => {
+  // 1) Get user from collection
+  const user = await User.findById(req.user.id).select('+password');
+
+  // 2) Check if POSTed current password is correct
+  if (!(await user.correctPassword(req.body.passwordCurrent)))
+    return next(new AppError('Your current password is wrong', 401));
+
+  // 3) If so, update password
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  await user.save();
+  // User.findByIdAndUpdate will NOT work as intended !
+
+  // 4) Log user in, send JWT
+  createSendToken(user, 200, res);
+});
+
+// ///////////////////////// Forgot and reset Process ///////////////////
+
+// 1. The user hitting forgot password from the client and provided an email address (In the backend: We take the email address and find them in our database to know who forgot their password.)
+
+// 2. We create a reset token and send as a link to the user via email. This is because we assume that email is secure and unique to the user. We include the reset token because each reset token is unique, so that we are able to find out who actually forgot their password, and we can change the person's password to the new one.
+
+// 3. When the user clicks the link and enter new password, validation occured and finally set a new password for the user. In the meantime, added a field to indicate at what time the user ever changed his password. This is used later when user try to access a protected route using the old token, we can block their access.
